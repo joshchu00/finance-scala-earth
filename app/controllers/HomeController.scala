7c659aa3ea084045da116ad2df98425598dc8a16
Josh Chu <josh.c@mig.me>
2016-12-28 22:49:03 +0800

    Initial play

---
+++ app/controllers/HomeController.scala
@@ -0,0 +1,13 @@
+package controllers
+
+import play.api.mvc.{Action, Controller}
+
+/**
+  * Created by joshchu999 on 12/14/16.
+  */
+class HomeController extends Controller {
+
+    def index = Action {
+      Ok("Hello World!")
+    }
+}
